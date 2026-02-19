57b08a5bd22e88259c166948fe22b2948fd709c1
Peter-Vegvari <vegvaripeter21@gmail.com>
2026-02-19 15:58:35 +0100

    wip2

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ source .venv/bin/activate
 
 sudo docker run -d --rm --name notconf-test -v $(pwd)/test/yang-modules:/yang-modules ghcr.io/notconf/notconf:21987110689-debug
 
-uv run netconf-console2 --host $(sudo docker inspect notconf-test --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}') --port 830 --get /modules-state
+uv run netconf-console2 --host 172.18.0.1 --port 830 --get /modules-state
--- src/netconf_manager/main.py
+++ src/netconf_manager/main.py
@@ -1,5 +1,25 @@
+from fastapi import FastAPI
+from netconf_client.connect import connect_ssh
+from netconf_client.ncclient import Manager
+
+# uv run fastapi dev src/netconf_manager/main.py
+app = FastAPI()
+host = "172.18.0.1"
+port = 830
+username = "admin"
+password = "admin"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
 def main():
-    
+    with connect_ssh(host, port, username, password) as session:
+        mgr = Manager(session, timeout=5)
+        print(mgr.get().data_xml)
+
 
 if __name__ == "__main__":
     main()
