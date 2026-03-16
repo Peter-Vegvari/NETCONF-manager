e25fd3fcb79254213a9a8bc62ac6ff264c0f9683
Peter-Vegvari <vegvaripeter21@gmail.com>
2026-03-16 23:39:04 +0100

    fixed uv sync

--- manager-backend/src/models.py
+++ manager-backend/src/models.py
@@ -10,7 +10,7 @@ class Connection(BaseModel):
     password: str = "admin"
 
     def download_schemas(self):
-        with manager.connect(
+        connection = manager.connect(
             host=self.host,
             port=self.port,
             username=self.user_name,
@@ -18,8 +18,13 @@ class Connection(BaseModel):
             hostkey_verify=False,
             device_params={"name": "default"},
             allow_agent=False,
-            look_for_keys=False,
-        ) as m:
+            look_for_keys=False
+        ) 
+
+        if connection is None:
+            return
+
+        with connection as m:
             filter_xml = """
                 <netconf-state xmlns="urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring">
                     <schemas/>
@@ -28,19 +33,19 @@ class Connection(BaseModel):
 
             reply = m.get(filter=("subtree", filter_xml))
 
-        tree = etree.fromstring(reply.xml.encode())
+            tree = etree.fromstring(reply.xml.encode())
 
-        ns = {"ncm": "urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring"}
+            ns = {"ncm": "urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring"}
 
-        schemas = tree.xpath("//ncm:schema", namespaces=ns)
+            schemas = tree.xpath("//ncm:schema", namespaces=ns)
 
-        for schema in schemas:
-            identifier = schema.find("ncm:identifier", ns).text
+            for schema in schemas:
+                identifier = schema.find("ncm:identifier", ns).text
 
-            print(f"Downloading {identifier}")
+                print(f"Downloading {identifier}")
 
-            schema_reply = m.get_schema(identifier=identifier)
+                schema_reply = m.get_schema(identifier=identifier)
 
-            filename = f"{identifier}.yang"
-            with open("resources/modules/" + filename, "w", encoding="utf-8") as f:
-                f.write(schema_reply.data)
+                filename = f"{identifier}.yang"
+                with open("resources/modules/" + filename, "w", encoding="utf-8") as f:
+                    f.write(schema_reply.data)
